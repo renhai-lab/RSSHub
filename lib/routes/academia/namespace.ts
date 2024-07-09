b482ac372bb05dce9c8b3627dd6a1cd6f1530da7
renhai-lab <3152498252@qq.com>
2024-07-09 15:09:40 +0800

    测试

--- lib/routes/academia/namespace.ts
+++
@@ -1,6 +0,0 @@
-import type { Namespace } from '@/types';
-
-export const namespace: Namespace = {
-    name: 'Academia',
-    url: 'https://www.academia.edu/',
-};
--- lib/utils/render.ts
+++
@@ -1,5 +0,0 @@
-export { default as rss3 } from '@/views/rss3';
-export { default as json } from '@/views/json';
-export { default as RSS } from '@/views/rss';
-export { default as Atom } from '@/views/atom';
-export { default as art } from 'art-template';
